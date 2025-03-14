277974b4251586195e7dd9a7b0025ea3d7a416ed
lolmageap <ekxk1234@naver.com>
2025-03-14 23:59:08 +0900

    refactor field name retrieval logic and add utility extension functions for type conversion

---
+++ test/src/test/kotlin/com/kmsl/dsl/util/UtilityExtension.kt
@@ -0,0 +1,9 @@
+package com.kmsl.dsl.util
+
+import java.math.BigDecimal
+
+internal fun Any?.toLong(): Long = this?.toString()?.replaceAfter(".", "")?.toLongOrNull() ?: 0
+internal fun Any?.toInt(): Int = this?.toString()?.replaceAfter(".", "")?.toIntOrNull() ?: 0
+internal fun Any?.toDouble(): Double = this?.toString()?.toDoubleOrNull() ?: 0.0
+internal fun Any?.toFloat(): Float = this?.toString()?.toFloatOrNull() ?: 0.0f
+internal fun Any?.toBigDecimal(): BigDecimal = this?.toString()?.toBigDecimalOrNull() ?: BigDecimal.ZERO
